3ffa5110ca2235baba913323be267d71e0a9ae1b
migue99angel <posadasarraez@correo.ugr.es>
2019-10-25 10:36:30 +0200

    Añadida la documentación de la clase Cono

--- cono.cc
+++ cono.cc
@@ -9,9 +9,14 @@ Cono::Cono(int num_vert_perf,int num_instancias_perf,float r,float h){
     float x;
     float y;
     perfil.push_back(Tupla3f(0,altura,0));
+    //Para obtener las cordenadas x e y he calculado la recta que pasa por los puntos 
+    //(0,h) y (r,0), he despejado la x y la he calculado dando valores, luego la
+    //sustituyo en la fórmula para la obtención de y y obtengo valores que están en la recta que 
+    //tomaré como perfil 
     for(int i = 0; i < num_vert_perf; ++i){
         x=-(r*i)/h;
         y=((-x)*h+1)/r;
+        //Si no hacemos esta comprobación tomará una recta hacia el infinito
         if(x<radio && y<altura)
          perfil.push_back(Tupla3f(x,y,0));
     }
